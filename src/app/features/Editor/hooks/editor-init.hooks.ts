7c3f521b28cdc1294fd143f2f38b00378b77c018
Lidia Kovac <70687866+LidiaKovac@users.noreply.github.com>
2025-12-18 02:23:33 +0100

    feat: add readability indexes (#41)
    * feat: add readability indexes
    
    * cleanup merge mess + fix css
    
    * fix code smells

--- src/app/features/Editor/hooks/editor-init.hooks.ts
+++ src/app/features/Editor/hooks/editor-init.hooks.ts
@@ -25,6 +25,7 @@ export const useQuillEditor = () => {
   // State to store current word count
   const [words, setWords] = useState<number>(0)
   const [loading, setLoading] = useState(false)
+  const [lix, setLix] = useState<Record<string, number> | null>(null)
   const [dialogueDensity, setDialogueDensity] = useState<number>(0)
 
   // Ref to the editor container div
@@ -35,6 +36,8 @@ export const useQuillEditor = () => {
   const highlightTimer = useRef<NodeJS.Timeout | null>(null)
   // Ref holding timer ID for debounced word count updates
   const wordCountTime = useRef<NodeJS.Timeout>(null)
+  //Ref holding timer ID for debounced ARI value
+  const ariTimer = useRef<NodeJS.Timeout>(null)
   // Ref holding timer ID for debounced dialogue density
   const dialogueDensityTimer = useRef<NodeJS.Timeout>(null)
   // Flag to track whether Quill instance was created
@@ -200,17 +203,51 @@ export const useQuillEditor = () => {
       //TODO: move to API and add highlighting of dialogue in subtle color + toggle
     }
 
+    const calculateReadabilityScore = () => {
+      if (ariTimer.current) clearTimeout(ariTimer.current)
+      // The formula for calculating the automated readability index is given below:
+      // 4.71 ( characters / words ) + 0.5 ( words / sentences ) − 21.43
+      // where characters is the number of letters and numbers, words is the number of spaces, and sentences is the number of sentences, which were counted manually by the typist when the above formula was developed. Non-integer scores are always rounded up to the nearest whole number, so a score of 10.1 or 10.6 would be converted to 11.
+      ariTimer.current = setTimeout(() => {
+        const text = quill.getText().trim()
+        const characters = text
+          .split("")
+          .filter((c) => /[a-zA-Z0-9]/.test(c)).length
+        const words = text.split(/\s+/).filter(Boolean).length || 1
+        const sentences = text.match(/(?:\.|\!|\?)/g)?.length ?? 1
+        const rawAri =
+          4.71 * (characters / words) + 0.5 * (words / sentences) - 21.43
+
+        //LIX = Läsbarhetsindex, indice di leggibilità
+        // Barn- och ungdomsböcker = 27
+        // Skönlitteratur = 33 letteratura e narrativa per adulti
+        // Dags- och veckopress = 39 giornali e informazione
+        // Saklitteratur = 47 letteratura scentifica
+        // Facklitteratur = 56 letteratura tecnica
+
+        const longWords = text
+          .split(/\s+/)
+          .filter(Boolean)
+          .filter((w) => w.length >= 6).length
+        //https://sv.wikipedia.org/wiki/L%C3%A4sbarhetsindex
+        const swedishindex = words / sentences + (longWords * 100) / words
+        setLix({ ari: Math.ceil(rawAri), lix: Math.ceil(swedishindex) }) // Round up per ARI spec
+      }, 500)
+    }
+    calculateReadabilityScore()
     calculateWordDensity()
     quill.on("text-change", calculateWordDensity)
-
+    quill.on("text-change", calculateReadabilityScore)
     return () => {
+      quill.off("text-change", calculateReadabilityScore)
+      if (ariTimer.current) clearTimeout(ariTimer.current)
+      ariTimer.current = null
       quill.off("text-change", calculateWordDensity)
       if (dialogueDensityTimer.current)
         clearTimeout(dialogueDensityTimer.current)
       dialogueDensityTimer.current = null
     }
-  }, [quill, words])
-
+  }, [words])
   // Effect to apply highlighting whenever rules change or the callback updates
   useEffect(() => {
     if (quillRef.current) {
@@ -219,5 +256,12 @@ export const useQuillEditor = () => {
   }, [Rules.getRules(), applyHighlightsCB])
 
   // Return editor div ref, current word count, and text content getter
-  return { editorRef, words, text: quill?.getText(), loading, dialogueDensity }
+  return {
+    editorRef,
+    words,
+    text: quill?.getText(),
+    loading,
+    lix,
+    dialogueDensity,
+  }
 }
