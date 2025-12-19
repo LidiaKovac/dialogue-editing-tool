85f177a424c413f833352f5950f61fbdfa59da4a
Lidia Kovac <70687866+LidiaKovac@users.noreply.github.com>
2025-12-19 01:22:57 +0100

    reorganize seo and jsonld for blog

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,21 +1,16 @@
-import { sanity } from "../../lib/sanity/sanity.config"
 import { PortableText } from "@portabletext/react"
 import { Metadata } from "next"
 import Link from "next/link"
-import Script from "next/script"
 import "../blog.scss"
+import {
+  getSingleBlogMetadataBySlug,
+  getSingleBlogPost,
+} from "@/app/lib/sanity/sanity.fn"
+import { JsonLD , metadata} from "./metadata"
 
 type Props = { params: { slug: string } }
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const [post] =
-    await sanity.fetch(`*[_type=="post" && slug.current=="${params.slug}"]
-    {
-      "date": publishedAt,
-      title,
-      slug,
-      "categories": categories[]->{title}.title,
-      "author": author->{name, "image": image.asset.url}
-    } | order(date desc)`)
+  const post = await getSingleBlogMetadataBySlug(params.slug)
 
   if (!post) {
     return {
@@ -24,34 +19,13 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     }
   }
 
-  return {
-    title: {
-      template: `%s | The Editing Blog`,
-      default: "The Editing Blog",
-    },
-    description: `Read "${post.title}" by ${post.author.name}. Categories: ${
-      post.categories?.join(", ") || "Uncategorized"
-    }.`,
-    openGraph: {
-      title: post.title,
-      description: `A blog post by ${
-        post.author.name
-      } in categories: ${post.categories?.join(", ")}`,
-      url: `https://editingthing.com/blog/${post.slug.current}`,
-      type: "article",
-      publishedTime: post.date,
-      authors: "Lidia Kovac",
-      tags: post.categories || [],
-      images: post.author.image ? [{ url: post.author.image }] : [],
-    },
-    keywords: post.categories || [],
-    twitter: {
-      card: "summary_large_image",
-      title: post.title,
-      description: `Read "${post.title}" by ${post.author.name}`,
-      images: post.author.image ? [post.author.image] : undefined,
-    },
-  }
+  return metadata(
+    post.title,
+    post.categories,
+    post.date,
+    post.slug.current,
+    post.author
+  )
 }
 export default async function BlogPost({
   params,
@@ -59,60 +33,8 @@ export default async function BlogPost({
   params: Promise<{ slug: string }>
 }>) {
   const { slug } = await params
-  const [post] = await sanity.fetch(`*[_type=="post" && slug.current=="${slug}"]
-    {
-      "date":publishedAt,
-      _id, 
-      title, slug,
-      "blocks": body[],
+  const post = await getSingleBlogPost(slug)
 
-      "categories": categories[]->{title}.title, 
-      "author": author->{"image": image{asset->{url}}.asset.url, name, bio[]{listItem, children[]{text, marks}, markDefs[]}}
-    } | order(date desc)`)
-  //breadcrumbs
-  const url = `https://editingthing.com/blog/${post.slug.current}`
-
-  const articleJsonLd = {
-    "@context": "https://schema.org",
-    "@type": "Article",
-    headline: post.title,
-    datePublished: post.date,
-    dateModified: post.date,
-    author: {
-      "@type": "Person",
-      name: post.author.name,
-    },
-    image: post.author.image ? [post.author.image] : undefined,
-    mainEntityOfPage: {
-      "@type": "WebPage",
-      "@id": url,
-    },
-  }
-
-  const breadcrumbJsonLd = {
-    "@context": "https://schema.org",
-    "@type": "BreadcrumbList",
-    itemListElement: [
-      {
-        "@type": "ListItem",
-        position: 1,
-        name: "Home",
-        item: "https://editingthing.com",
-      },
-      {
-        "@type": "ListItem",
-        position: 2,
-        name: "Blog",
-        item: "https://editingthing.com/blog",
-      },
-      {
-        "@type": "ListItem",
-        position: 3,
-        name: post.title,
-        item: url,
-      },
-    ],
-  }
   if (!post)
     return (
       <div className="blog">
@@ -122,17 +44,13 @@ export default async function BlogPost({
     )
   return (
     <div className="blog">
-      {/* JSON‑LD for SEO */}
-      <Script
-        id="article-jsonld"
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(articleJsonLd) }}
+      <JsonLD
+        title={post.title}
+        date={post.date}
+        author={post.author}
+        url={`https://editingthing.com/blog/${post.slug.current}`}
       />
-      <Script
-        id="breadcrumb-jsonld"
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(breadcrumbJsonLd) }}
-      />
+
       {/* <h2>The Editing Blog</h2> */}
       <h1>{post.title}</h1>
 
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,59 +3,30 @@ import { sanity } from "../lib/sanity/sanity.config"
 import { Metadata } from "next";
 import "./blog.scss"
 import { fontBlog } from "../lib/fonts";
-
-
+import metadata from "./metadata"
+import { getAllBlogPosts, getBlogMetadata } from "../lib/sanity/sanity.fn"
 
 export async function generateMetadata(): Promise<Metadata> {
-  const posts =
-    await sanity.fetch(`*[_type=="post" && dateTime(publishedAt) < dateTime(now())]
-    {
-      title,
-      slug,
-      "categories": categories[]->{title}.title,
-      "author": author->{name}
-    } | order(publishedAt desc)`)
+  const posts = await getBlogMetadata()
 
   const categories = posts.flatMap(
     (post: Record<string, any>) => post.categories || []
   )
-  const uniqueCategories = Array.from(new Set(categories))
+  const uniqueCategories: string[] = Array.from(new Set(categories))
 
-  return {
-    title: "The Editing Blog",
-    description:
-      "Latest blog posts covering various categories including " +
-      (uniqueCategories.join(", ") || "no categories"),
-    openGraph: {
-      title: "The Editing Blog - Latest Posts",
-      description:
-        "Read the latest blog posts across categories: " +
-        (uniqueCategories.join(", ") || "no categories"),
-      url: "https://editingthing.com/blog",
-      type: "website",
-      images: posts[0]?.author.image
-        ? [{ url: posts[0].author.image, alt: posts[0].author.name }]
-        : [],
-    },
-    keywords: uniqueCategories as string[],
-    twitter: {
-      card: "summary_large_image",
-      title: "The Editing Blog - Latest Posts",
-      description: "Stay updated with the latest posts on The Editing Blog.",
-    },
-  }
+  const categoryString =
+    uniqueCategories.length > 0
+      ? uniqueCategories.join(", ")
+      : "editing, dialogue, and fanfiction writing"
+
+  const url = "https://editingthing.com/blog"
+
+  return metadata(categoryString, url, uniqueCategories)
 }
+
 export default async function BlogLanding() {
-  const posts =
-    await sanity.fetch(`*[_type=="post" && dateTime(publishedAt) < dateTime(now())]
-    {
-      "date":publishedAt,
-      _id, 
-      title, slug,
-      "categories": categories[]->{title}.title, 
-      "author": author->{"image": image{asset->{url}}.asset.url, name, bio[]{listItem, children[]{text, marks}, markDefs[]}}
-    } | order(date desc)`)
-  //      "blocks": body[]{listItem, children[]{text, marks},asset->{url}, markDefs[], },
+  const posts = await getAllBlogPosts()
+
   /* 
     La fetch da indietro .result, ma la variabile e' SOLO i risultati, 
     non quello che si vede in network tab
